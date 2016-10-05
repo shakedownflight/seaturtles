bcb8c92cd3a4d3c203828e8d482e019c3a1c85e5
benefitjoo <benefit.joo@gmila.com>
2016-10-05 17:30:41 +0900

    basic config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './client/src/index.js',
+ 
+    output: {
+        path: __dirname + '/client/public/',
+        filename: 'bundle.js'
+    },
+ 
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/client/public/'
+    },
+ 
+    module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /node_modules/,
+                    query: {
+                        cacheDirectory: true,
+                        presets: ['es2015', 'react']
+                    }
+                }
+            ]
+        }
+};
