1f8ab74dba345debb7e3ceb706553095b8b668e6
foxtrip <ambrosia012@gmail.com>
2016-10-07 17:02:54 +0900

    contents render

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,39 +1,33 @@
 import React from 'react';
 import Header from './Header';
+import List from './List';
 import Contents from './Contents';
 import Navi from './Navi';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import { Nav } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { NavItem } from 'react-bootstrap';
-
+// import contentsData  from './../../public/db/contentsData';
 
 class App extends React.Component {
-    constructor(props) {   
+   constructor(props) {   
       super(props);                //---> props를 React.Component에 전달한다.   
       this.state = {
         head:window.headData,
-        list:window.contentsData // 일단 불변이라고 둔다.
+        list:window.contentsData // []임. 
       }; 
     }
     render(){ 
         return (
           <div>
             <div>
-              <Header source = {this.state.head} /> 
+              <Header source = {this.state.head}/> 
             </div>
-            <Nav justified>
-              <NavItem href="">엄빠순</NavItem>
-              <NavItem href="">Link</NavItem>
-              <NavItem href="">Link</NavItem>
-              <NavItem href="">가격순</NavItem>
-            </Nav>
+            <div>
+              <List />
+            </div>
             <div>
               <Contents source = {this.state.list}/>
             </div>
             <div>
-                <Navi/>
-             </div>
+              <Navi/>
+            </div>
           </div>
         );
     }
--- client/src/components/Contents.js
+++ client/src/components/Contents.js
@@ -1,18 +1,20 @@
 import React from 'react';
+//import { Scrollspy }  from 'react-scroll';//'react-scrollspy';
+import ContentsEntry from './ContentsEntry';
 
 class Contents extends React.Component {
     constructor(props) {
       super(props);
-    //   this.connectSearch = this.connectSearch.bind(this); 
     }
     render(){
         return  (
-        <div className="btn-group">
-          <button type="button" className="btn btn-default">Left</button>
-          <button type="button" className="btn btn-default">Middle</button>
-          <button type="button" className="btn btn-default">Right</button>
-          <button type="button" className="btn btn-default">Right2</button>
-        </div>
+          <div >
+              { this.props.source.map((chocolate, i) => {
+                return (
+                      <ContentsEntry value={chocolate} key= {i}/>
+                );
+               })}
+          </div>
         );
     }
 };
